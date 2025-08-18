4eeeca164bedcc645897825fc465a7565d748d6d
Charles Owusu Bih <charlesowusubih@gmail.com>
2025-08-18 08:30:38 +0000

    dd

--- api/routes/students.py
+++ api/routes/students.py
@@ -30,7 +30,7 @@ def create_student(
 def list_students():
     conn = get_db_connection()
     rows = conn.execute(
-        "SELECT id, student_name, class, index_number FROM students"
+        "SELECT * FROM students"
     ).fetchall()
 
     # Rows are dict-like in libsql-client
