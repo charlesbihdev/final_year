5a3f2806082d458be436158225b09fc2e457c8c6
Charles Owusu Bih <charlesowusubih@gmail.com>
2025-08-21 04:18:59 +0000

    hhhhhljjkjkl

--- frontend/components/ui/form-field.tsx
+++ frontend/components/ui/form-field.tsx
@@ -1,6 +1,5 @@
 import { Label } from "@/components/ui/label"
 import { Input } from "@/components/ui/input"
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select"
 
 interface FormFieldProps {
   label: string
